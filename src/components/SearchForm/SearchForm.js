5f050e379762ca08b286c497b17649d9b25566ca
darya55k <darya55k2001@yandex.ru>
2021-09-30 00:04:24 +0300

    rev

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -6,15 +6,19 @@ function SearchForm() {
     return (
         <section className="search">
             <form className="search__form">
+                <div className="search__films">
                 <img src={search__icon} alt="Поиск" className="search__icon"/>
                 <fieldset className="search__form-fields">
                     <input type="text" placeholder="Фильм" className="search__form-input" required/>
                 </fieldset>
+                </div>
+                <div className="search__box">
                 <button className="search__form-button" type="submit">Найти</button>
                 <span className="search__toggle-border" />
                 <div className="search__toggle-box">
                     <FilterCheckbox />
                     <h3 className="search__toggle-text">Короткометражки</h3>
+                </div>
                 </div>
 
             </form>
